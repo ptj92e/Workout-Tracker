71c9af26667165fbd931e8ca05d15b9e0df5f527
Phillip Jones <ptj92e@gmail.com>
2020-03-07 19:15:38 -0600

    Added Heroku Environme tal Variable

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false
 });
